a0a70164839ca35b66ba88c19c26ec96a9f241f4
Jules02 <julesdupont02@gmail.com>
2024-09-24 15:14:54 +0200

    Create CMake project

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Jules Dupont on 24/09/2024.
+//
+
+#include "helloWorld.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ helloWorld.h
@@ -0,0 +1,16 @@
+//
+// Created by Jules Dupont on 24/09/2024.
+//
+
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
+
+class helloWorld {
+
+};
+
+
+
+#endif //HELLOWORLD_H
