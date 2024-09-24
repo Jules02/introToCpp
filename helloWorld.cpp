4ec3ad27688b59660e319b2ffba7a1410037f5fe
Jules02 <julesdupont02@gmail.com>
2024-09-24 15:21:03 +0200

    Print integers in custom order around Hello World

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -7,6 +7,12 @@
 #include <iostream>
 
 int main() {
+    for (int i = 2; i <= 10; i+=2) {
+        std::cout << i << std::endl;
+    }
     std::cout << "Hello World!\n";
+    for (int i = 9; i >= 1; i-=2) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
