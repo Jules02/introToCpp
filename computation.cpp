2b2d64552e8e7e2e9b1c1e1eebcb8cc2fe1f3347
Jules02 <julesdupont02@gmail.com>
2024-09-26 10:58:48 +0200

    Correctly implement Fibonacci, improve notes and add slides

--- computation.cpp
+++ computation.cpp
@@ -7,13 +7,21 @@
 
 int arrayAverage(int array[], int size);
 
+int fibonacci(int n);
+
 int main() {
 
+    /*
     int MyArray[5] = {3, 6, 7, 90, 100};
 
     int avg_MyArray = arrayAverage(MyArray, 5);
 
     std::cout << avg_MyArray << std::endl;
+    */
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << fibonacci(i) << std::endl;
+    }
 
     return 0;
 }
@@ -24,4 +32,19 @@ int arrayAverage(int array[], int size) {
         sum += array[i];
     }
     return sum / size;
+}
+
+int fibonacci(int n) {
+    int fib0 = 0;
+    if (n == 0) { return fib0; }
+    int fib1 = 1;
+    if (n == 1) { return fib1; }
+    int fib_n_moinsUn = fib0;
+    int fib_n = fib1;
+    for (int i = 2; i <= n; ++i) {
+        const int tmp = fib_n;
+        fib_n = fib_n_moinsUn + fib_n;
+        fib_n_moinsUn = tmp;
+    }
+    return fib_n;
 }
