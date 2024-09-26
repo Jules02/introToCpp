5ba113d46f47b71f32b0459064cc5fdc4c5180cb
Jules02 <julesdupont02@gmail.com>
2024-09-26 11:12:23 +0200

    Use float instead of int for calculating array avg + refactor

--- computation.cpp
+++ computation.cpp
@@ -5,19 +5,15 @@
 #include <iostream>
 #include <ostream>
 
-int arrayAverage(int array[], int size);
+float arrayAverage(const int array[], int size);
 
 int fibonacci(int n);
 
 int main() {
 
-    /*
     int MyArray[5] = {3, 6, 7, 90, 100};
-
-    int avg_MyArray = arrayAverage(MyArray, 5);
-
+    float avg_MyArray = arrayAverage(MyArray, 5);
     std::cout << avg_MyArray << std::endl;
-    */
 
     for (int i = 0; i < 10; i++) {
         std::cout << fibonacci(i) << std::endl;
@@ -26,12 +22,13 @@ int main() {
     return 0;
 }
 
-int arrayAverage(int array[], int size) {
-    int sum = 0;
+float arrayAverage(const int array[], int size) {
+    float sum = 0;
     for (int i = 0; i < size; i++) {
-        sum += array[i];
+        sum += (float) array[i];
     }
-    return sum / size;
+    float avg = sum / (float) size;
+    return avg;
 }
 
 int fibonacci(int n) {
@@ -39,8 +36,10 @@ int fibonacci(int n) {
     if (n == 0) { return fib0; }
     int fib1 = 1;
     if (n == 1) { return fib1; }
+
     int fib_n_moinsUn = fib0;
     int fib_n = fib1;
+
     for (int i = 2; i <= n; ++i) {
         const int tmp = fib_n;
         fib_n = fib_n_moinsUn + fib_n;
