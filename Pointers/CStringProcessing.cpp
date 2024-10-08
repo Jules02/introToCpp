9303e01c7f2aff955d157c1932a45e40a02d6fb1
Jules02 <julesdupont02@gmail.com>
2024-10-08 16:32:06 +0200

    Implement concatenation method for end of exercise 1

--- Pointers/CStringProcessing.cpp
+++ Pointers/CStringProcessing.cpp
@@ -8,7 +8,7 @@
 
 int mylen(const char* s) {
   int len = 0;
-  while (s[len] != '\0') { len++; }
+  while (*(s+len) != '\0') { len++; }
   return len;
 }
 
@@ -16,11 +16,21 @@ void inverse(char* s) {
   int len = mylen(s);
   int i = 0, j = len-1;
   while (i < j) {
-    char t = s[i];
-    s[i] = s[j];
-    s[j] = t;
+    char t = *(s+i);
+    *(s+i) = *(s+j);
+    *(s+j) = t;
     ++i; --j;
   }
+}
+
+char* concatenate(char *s1, char *s2) {
+  int len1 = mylen(s1); int len2 = mylen(s2);
+  int len = len1 + len2;
+  char* ptr = new char[len+1];
+  for (int i = 0; i < len1; i++) { *(ptr+i) = *(s1+i); }
+  for (int j = 0; j < len2; j++) { *(ptr+len1+j) = *(s2+j); }
+  *(ptr+len) = '\0';
+  return ptr;
 }
 
 int main() {
@@ -29,5 +39,9 @@ int main() {
 
   inverse(str);
   std::cout << "Its inverse is '" << str << "'." << std::endl;
+
+  char *str2 = concatenate(str, str);
+  std::cout << str2 << std::endl;
+  delete[] str2;
   return 0;
 }
